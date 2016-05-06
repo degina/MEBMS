62daf8682ceb14e791709c83027693b9fed9862e
nyamjantsan <nyamjantsannyamtseren@gmail.com>
2016-05-06 11:54:41 +0900

    5/6

--- app/src/main/java/com/example/mebms/mebms/HomeFragment.java
+++ app/src/main/java/com/example/mebms/mebms/HomeFragment.java
@@ -1,15 +1,22 @@
 package com.example.mebms.mebms;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class HomeFragment extends Fragment {
-	
+
+	private Button uvchinListBtn;
+	private Button sergiileltListBtn;
+	private Button emchilgeeListBtn;
+	private Button shinjilgeeListBtn;
+	private Button shiljiltListBtn;
 	// TODO: Rename and change types and number of parameters
 	public static HomeFragment newInstance() {
 		HomeFragment fragment = new HomeFragment();
@@ -25,8 +32,65 @@ public class HomeFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_home, container,  
-				    false);  
-			  return rootView;
+				    false);
+		uvchinListBtn = (Button) rootView.findViewById(R.id.uvchin_list_btn);
+		sergiileltListBtn = (Button) rootView.findViewById(R.id.sergiilelt_list_btn);
+		emchilgeeListBtn = (Button) rootView.findViewById(R.id.emchilgee_list_btn);
+		shinjilgeeListBtn = (Button) rootView.findViewById(R.id.shinjilgee_list_btn);
+		shiljiltListBtn = (Button) rootView.findViewById(R.id.shiljilt_list_btn);
+
+		uvchinListBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+				FragmentManager fragmentManager = getFragmentManager();
+				fragmentManager.beginTransaction()
+						.replace(R.id.frame_container, ListUvchinFragment.newInstance())
+						.commit();
+			}
+		});
+		sergiileltListBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+				FragmentManager fragmentManager = getFragmentManager();
+				fragmentManager.beginTransaction()
+						.replace(R.id.frame_container, ListSergiileltFragment.newInstance())
+						.commit();
+			}
+		});
+		emchilgeeListBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+//				FragmentManager fragmentManager = getFragmentManager();
+//				fragmentManager.beginTransaction()
+//						.replace(R.id.frame_container, ListEmchilgeeFragment.newInstance())
+//						.commit();
+			}
+		});
+		shinjilgeeListBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+				FragmentManager fragmentManager = getFragmentManager();
+				fragmentManager.beginTransaction()
+						.replace(R.id.frame_container, ListShinjilgeeFragment.newInstance())
+						.commit();
+			}
+		});
+		shiljiltListBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+//				FragmentManager fragmentManager = getFragmentManager();
+//				fragmentManager.beginTransaction()
+//						.replace(R.id.frame_container, ListShiljiltFragment.newInstance())
+//						.commit();
+			}
+		});
+
+		return rootView;
 	}
 
 
